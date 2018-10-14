3d8594e5e7ee3371de2e91b8549bd6a7f88257ca
cikciks <cikciks@gmail.com>
2018-10-14 18:50:53 +0700

    1

--- project-dr/models/project_dr.py
+++ project-dr/models/project_dr.py
@@ -15,7 +15,7 @@ class ProjectTaskSubtask(models.Model):
     _inherit = "project.task.subtask"
     result_type = fields.Selection([(k, v) for k, v in list(RESULT_TYPES.items())],
                              'Analysis Type', required=True, copy=False, default='material')
-    reference = fields.Char(required=True, string="Reference")
-    specification = fields.Char(required=True, string="Specification")
-    result = fields.Char(required=True, string="Result")
-    remark = fields.Char(required=True, string="Remark")
+    reference = fields.Char(required=False, string="Reference")
+    specification = fields.Char(required=False, string="Specification")
+    result = fields.Char(required=False, string="Result")
+    remark = fields.Char(required=False, string="Remark")
