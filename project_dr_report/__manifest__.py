9a3b18f444ee98351e6d09d0776acc19f41b72af
cikciks <cikciks@gmail.com>
2018-10-14 21:00:59 +0700

    1

--- project_dr_report/__manifest__.py
+++ project_dr_report/__manifest__.py
@@ -1,18 +1,28 @@
-
-# -*- coding: utf-8 -*-
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-
 {
-    "name": "Project Document Release Report",
-    "summary": "Report for Document Release.",
+    "name": """Project Task - Document Release Report""",
+    "summary": """Report for Document Release""",
+    "category": """Project Management""",
+    "images": ['images/checklist_main.png'],
     "version": "11.0.1.0.0",
+    "application": False,
+
     "author": "MCR",
-    "category": "Project Management",
-    "depends": ['project-dr'],,
+
+    "depends": ['base', 'project','project_task_subtask'],
+    "external_dependencies": {"python": [], "bin": []},
     "data": [
         'views/release_report.xml'
     ],
-    "license": "AGPL-3",
-    'installable': True,
-    'application': False,
+    "qweb": [
+    ],
+    "demo": [
+        ''
+    ],
+
+    "post_load": None,
+    "pre_init_hook": None,
+    "post_init_hook": None,
+
+    "auto_install": False,
+    "installable": True,
 }
