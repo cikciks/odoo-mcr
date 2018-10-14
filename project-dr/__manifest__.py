0cad2e84b93594d05cb54162c7f96701e2a857ff
cikciks <cikciks@gmail.com>
2018-10-14 15:42:50 +0700

    1

---
+++ project-dr/__manifest__.py
@@ -0,0 +1,28 @@
+{
+    "name": """Project Task additional field""",
+    "summary": """add field for document released""",
+    "category": """Project Management""",
+    "images": ['images/checklist_main.png'],
+    "version": "11.0.1.0.0",
+    "application": False,
+
+    "author": "cikciks",
+
+    "depends": ['base', 'project','project_task_subtask'],
+    "external_dependencies": {"python": [], "bin": []},
+    "data": [
+        'views/project_dr.xml'
+    ],
+    "qweb": [
+    ],
+    "demo": [
+        'demo/project_dr_demo.xml'
+    ],
+
+    "post_load": None,
+    "pre_init_hook": None,
+    "post_init_hook": None,
+
+    "auto_install": False,
+    "installable": True,
+}
