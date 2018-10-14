fa926ab05542e2adf569bfb4ddd3223894ec7373
cikciks <cikciks@gmail.com>
2018-10-14 20:51:36 +0700

    1

---
+++ project_dr_report/__manifest__.py
@@ -0,0 +1,18 @@
+
+# -*- coding: utf-8 -*-
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Project Document Release Report",
+    "summary": "Report for Document Release.",
+    "version": "11.0.1.0.0",
+    "author": "MCR",
+    "category": "Project Management",
+    "depends": ['project-dr'],,
+    "data": [
+        'views/release_report.xml'
+    ],
+    "license": "AGPL-3",
+    'installable': True,
+    'application': False,
+}
