e7a5a2148978045591ad0aea89a4295cf61eb18e
cikciks <cikciks@gmail.com>
2018-10-14 17:06:30 +0700

    1

--- project-dr/models/project_dr.py
+++ project-dr/models/project_dr.py
@@ -5,6 +5,7 @@ from odoo.tools.translate import _
 
 class ProjectSubTaskDR(models.Model):
     _name = "project.subtask.dr"
+    _inherit = "project.task.subtask"
     reference = fields.Char(required=True, string="Reference")
     specification = fields.Char(required=True, string="Specification")
     result = fields.Char(required=True, string="Result")
