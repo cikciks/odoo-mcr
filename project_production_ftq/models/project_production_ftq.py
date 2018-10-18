14b2e97befff3e796bd60b10a55e200b2761b449
cikciks <cikciks@gmail.com>
2018-10-18 13:53:52 +0700

    1

--- project_production_ftq/models/project_production_ftq.py
+++ project_production_ftq/models/project_production_ftq.py
@@ -7,3 +7,13 @@ class ProductionFTQ(models.Model):
     area = fields.Char(required=False, string="Area")
     state = fields.Char(required=False, string="State")
     note = fields.Char(required=False, string="Note")
+    project_id = fields.Many2one("project.project", related='task_id.project_id', store=True)
+    user_id = fields.Many2one('res.users', 'Assigned to', required=True)
+    task_id = fields.Many2one('project.task', 'Task', ondelete='cascade', required=True, index="1")
+
+
+class Task(models.Model):
+    _inherit = "project.task"
+    subtask_ids = fields.One2many('project.production.ftq', 'task_id', 'Subtask')
+    kanban_subtasks = fields.Text(compute='_compute_kanban_subtasks')
+    default_user = fields.Many2one('res.users', compute='_compute_default_user')
