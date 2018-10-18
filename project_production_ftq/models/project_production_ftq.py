f5e0b6a5ccfd9d807471ccf8c9de92ad6a3192f5
cikciks <cikciks@gmail.com>
2018-10-18 13:28:36 +0700

    1

--- project_production_ftq/models/project_production_ftq.py
+++ project_production_ftq/models/project_production_ftq.py
@@ -3,6 +3,3 @@ from odoo import api, fields, models
 class ProductionFTQ(models.Model):
     _name = "production.ftq"
 
-    @api.model
-    def ftq_list(self):
-        self.create(dict(name='demo.webkul.com'))
