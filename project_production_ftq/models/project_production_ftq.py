87ce0aee912d20ce06a02e0fe9aa544b24468b1b
cikciks <cikciks@gmail.com>
2018-10-18 14:52:40 +0700

    1

--- project_production_ftq/models/project_production_ftq.py
+++ project_production_ftq/models/project_production_ftq.py
@@ -8,7 +8,6 @@ class ProductionFTQ(models.Model):
     state = fields.Char(required=False, string="State")
     note = fields.Char(required=False, string="Note")
     project_id = fields.Many2one("project.project", related='task_id.project_id', store=True)
-    user_id = fields.Many2one('res.users', 'Assigned to', required=True)
     task_id = fields.Many2one('project.task', 'Task', ondelete='cascade', required=True, index="1")
 
 
