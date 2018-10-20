a0dfc417025b45b10f7eb65f59d9349257cc85e1
cikciks <cikciks@gmail.com>
2018-10-20 11:04:58 +0700

    1

--- project_production/models/project_production.py
+++ project_production/models/project_production.py
@@ -21,6 +21,8 @@ class ProjectTask(models.Model):
     weight_box = fields.Float(required=False, string="Weight per Box (kg)")
     qty_in_box = fields.Integer(required=False, string="Quantity in Box")
     qty_in_pack = fields.Integer(required=False, string="Quantity in Pack")
+    qty_sample = fields.Float(required=False, string="Sample Quantity (kg)")
+    weight_sample = fields.Integer(required=False, string="Sample Weight")
     date_prod = fields.Date(required=True, string="Production Date", default=fields.Date.today)
     date_rel_deadline = fields.Date(required=False, string="Release Deadline")
     date_release = fields.Date(required=False, string="Release Date", compute='_compute_release')
