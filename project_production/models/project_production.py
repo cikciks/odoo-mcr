79f4ae6a64d5e5e1d8f7b5d29df09251979dff47
cikciks <cikciks@gmail.com>
2018-10-21 17:06:42 +0700

    1

--- project_production/models/project_production.py
+++ project_production/models/project_production.py
@@ -98,7 +98,7 @@ class ProductionProduct(models.Model):
     code = fields.Char(string='Code', required=True)
     weight_pack = fields.Float(required=True, string="Weight per Pack (kg)")
     qty_box = fields.Float(required=True, string="Qty in Box (pack)")
-    weight_box = fields.Float(required=False, string="Weight per Box (kg)", compute='_compute_weight_box')
+    weight_box = fields.Float(required=False, string="Weight per Box (kg)")
 
     @api.onchange('qty_box', 'weight_pack')
     def _compute_weight_box(self):
