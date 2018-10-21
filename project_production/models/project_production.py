54d17ed8cbf6b669a2254369ddca27880ba9caab
cikciks <cikciks@gmail.com>
2018-10-21 20:36:46 +0700

    1

--- project_production/models/project_production.py
+++ project_production/models/project_production.py
@@ -24,6 +24,7 @@ class ProjectTask(models.Model):
     weight_box = fields.Float(required=False, string="Weight per Box (kg)", compute='_compute_weight_box')
     qty_in_box = fields.Integer(required=False, string="Quantity in Box")
     qty_in_pack = fields.Integer(required=False, string="Quantity in Pack")
+    total_weight_fg = fields.Float(required=False, string="Total Weight FG(kg)", compute='_compute_total_weight_fg')
     qty_sample = fields.Integer(required=False, string="Sample Qty (pack)")
     weight_sample = fields.Float(required=False, string="Sample Weight (kg)")
     weight_rm = fields.Float(required=False, string="Raw Material (kg)")
@@ -51,6 +52,12 @@ class ProjectTask(models.Model):
     @api.depends('product_ids')
     def _compute_weight_pack(self):
             self.weight_pack = self.product_ids.weight_pack
+
+    @api.depends('weight_pack','weight_box','qty_in_box','qty_in_pack')
+    def _compute_weight_pack(self):
+        total_weight_pack = self.weight_pack * self.qty_in_pack
+        total_weight_box = self.weight_box * self.qty_in_box
+        self.total_weight_fg = total_weight_box + total_weight_pack
 
     @api.onchange('date_prod','shelf_life')
     def _compute_bb(self):
