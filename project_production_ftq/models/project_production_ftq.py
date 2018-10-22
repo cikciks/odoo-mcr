ae0a3659191c5087985fbdbe6b642c7d290a541c
cikciks <cikciks@gmail.com>
2018-10-22 12:29:56 +0700

    1

--- project_production_ftq/models/project_production_ftq.py
+++ project_production_ftq/models/project_production_ftq.py
@@ -16,9 +16,10 @@ class ProductionFTQ(models.Model):
 
     @api.multi
     def _compute_check_point(self):
-        if self.check_point:
-            record = self.env['ftq.checkpoint'].browse[self.parameter.checkpoint_ids]
-            record.write('check_point': 'id')
+        for record in self:
+            if self.check_point:
+                record = self.env['ftq.checkpoint'].browse[self.parameter.checkpoint_ids]
+                record.write('check_point': id())
 
 
 class Task(models.Model):
