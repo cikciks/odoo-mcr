761907d541742f69b4c2fe0a2b8a30ed71878d55
cikciks <cikciks@gmail.com>
2018-10-22 12:11:29 +0700

    1

--- project_production_ftq/models/project_production_ftq.py
+++ project_production_ftq/models/project_production_ftq.py
@@ -8,7 +8,7 @@ class ProductionFTQ(models.Model):
 
     # name = fields.Char(required=False, string="Parameter")
     parameter = fields.Many2one('ftq.parameter', string='Parameter')
-    check_point = fields.Many2one('ftq.checkpoint', compute='_compute_check_point', string='Check Point')
+    check_point = fields.Many2one('ftq.checkpoint', compute='_compute_checkpoint', string='Check Point')
     state = fields.Char(required=False, string="State")
     note = fields.Char(required=False, string="Note")
     project_id = fields.Many2one("project.project", related='task_id.project_id', store=True)
