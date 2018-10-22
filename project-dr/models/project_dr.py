2ac43fe3aaf800424b5de5d9f1d525304f4b8eb1
cikciks <cikciks@gmail.com>
2018-10-22 15:33:56 +0700

    1

--- project-dr/models/project_dr.py
+++ project-dr/models/project_dr.py
@@ -13,7 +13,8 @@ RESULT_TYPES = {'material': 'Incoming Analysis Results',
 
 class ProjectTaskSubtask(models.Model):
     _inherit = "project.task.subtask"
-    name = fields.Many2one('dr.parameter', string='Parameter')
+    name = fields.Char(required=True, string="Description", compute)
+    parameter = fields.Many2one('dr.parameter', string='Parameter')
     result_type = fields.Selection([(k, v) for k, v in list(RESULT_TYPES.items())],
                              'Analysis Type', required=True, copy=False, default='material')
     reference = fields.Char(required=False, string="Reference")
@@ -21,30 +22,7 @@ class ProjectTaskSubtask(models.Model):
     result = fields.Char(required=False, string="Result")
     remark = fields.Char(required=False, string="Remark")
 
-    @api.multi
-    def write(self, vals):
-        old_names = dict(list(zip(self.mapped('id'), self.mapped('name'))))
-        result = super(ProjectTaskSubtask, self).write(vals)
-        for r in self:
-            if vals.get('state'):
-                r.task_id.send_subtask_email(r.name.id, r.state, r.reviewer_id.id, r.user_id.id)
-                if self.env.user != r.reviewer_id and self.env.user != r.user_id:
-                    raise UserError(_('Only users related to that subtask can change state.'))
-            if vals.get('name'):
-                r.task_id.send_subtask_email(r.name.id, r.state, r.reviewer_id.id, r.user_id.id, old_name=old_names[r.id])
-                if self.env.user != r.reviewer_id and self.env.user != r.user_id:
-                    raise UserError(_('Only users related to that subtask can change state.'))
-            if vals.get('user_id'):
-                r.task_id.send_subtask_email(r.name.id, r.state, r.reviewer_id.id, r.user_id.id)
-        return result
 
-    @api.model
-    def create(self, vals):
-        result = super(ProjectTaskSubtask, self).create(vals)
-        vals = self._add_missing_default_values(vals)
-        task = self.env['project.task'].browse(vals.get('task_id'))
-        task.send_subtask_email(vals['name'], vals['state'], vals['reviewer_id'], vals['user_id'])
-        return result
 
 class DRParameter(models.Model):
     _name = 'dr.parameter'
