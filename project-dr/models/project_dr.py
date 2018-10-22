2b6d0ce3ff98970d8e1c58296dd6fc7b92654db8
cikciks <cikciks@gmail.com>
2018-10-22 15:10:29 +0700

    1

--- project-dr/models/project_dr.py
+++ project-dr/models/project_dr.py
@@ -38,12 +38,12 @@ class ProjectTaskSubtask(models.Model):
                 r.task_id.send_subtask_email(r.name.name, r.state, r.reviewer_id.id, r.user_id.id)
         return result
 
-        @api.model
-        def create(self, vals):
-            result = super(ProjectTaskSubtask, self).create(vals)
-            vals = self._add_missing_default_values(vals)
-            task = self.env['project.task'].browse(vals.get('task_id'))
-            task.send_subtask_email(vals['name.name'], vals['state'], vals['reviewer_id'], vals['user_id'])
+    @api.model
+    def create(self, vals):
+        result = super(ProjectTaskSubtask, self).create(vals)
+        vals = self._add_missing_default_values(vals)
+        task = self.env['project.task'].browse(vals.get('task_id'))
+        task.send_subtask_email(vals['name.name'], vals['state'], vals['reviewer_id'], vals['user_id'])
         return result
 
 class DRParameter(models.Model):
