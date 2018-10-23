70a4472eab5eb27def9fc458dfeca96ac1f3e4df
cikciks <cikciks@gmail.com>
2018-10-23 09:53:59 +0700

    1

--- project_production_dr/models/project_production_dr.py
+++ project_production_dr/models/project_production_dr.py
@@ -26,8 +26,8 @@ class ProductionDR(models.Model):
     # result_type = fields.Selection([(k, v) for k, v in list(RESULT_TYPES.items())],
     #                         'Analysis Type', required=True, copy=False, default='material')
     reference = fields.Char(required=False, string="Reference")
-    specification = fields.Many2one('dr.parameter', compute='_compute_specification', string='Specification')
-    # specification = fields.Char(required=False, string="Specification")
+    # specification = fields.Many2one('dr.parameter', compute='_compute_specification', string='Specification')
+    specification = fields.Char(required=False, compute='_compute_specification', string="Specification")
     result = fields.Char(required=False, string="Result")
     remark = fields.Char(required=False, string="Remark")
     reviewer_id = fields.Many2one('res.users', 'Created by', readonly=True, default=lambda self: self.env.user)
