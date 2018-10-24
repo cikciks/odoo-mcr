b37c72998359f99151c0fc10bb1daa569b5ff71b
cikciks <cikciks@gmail.com>
2018-10-24 11:12:36 +0700

    1

--- project_production_ftq/models/project_production_ftq.py
+++ project_production_ftq/models/project_production_ftq.py
@@ -44,6 +44,7 @@ class Task(models.Model):
     @api.one
     @api.depends('ftq_ids')
     def _count_total_parameter(self):
+        task_id = self.ftq_ids.task_id.id
         self.total_parameter = self.ftq_ids.search_count([])
 
     @api.one
