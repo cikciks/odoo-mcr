04c2886762fd830af4476d90d035616d371b4693
cikciks <cikciks@gmail.com>
2018-10-25 15:41:06 +0700

    1

--- project_production_ftq/models/project_production_ftq.py
+++ project_production_ftq/models/project_production_ftq.py
@@ -56,7 +56,7 @@ class Task(models.Model):
         # parameter = self.ftq_ids.search([('task_id', '=', task_id)])
         parameter = self.ftq_ids.filtered(lambda r: r.task_id == task_id)
         # self.total_point = parameter.search_count([('mark_point', '=', True)])
-        self.total_point = parameter.search_count([])
+        self.total_point = task_id
         # self.total_point = self.ftq_ids.search_count([[('task_id', '=', task_id)], [('mark_point', '=', True)]])
 
     @api.depends('total_parameter','total_point')
